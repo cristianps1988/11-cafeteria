328ed7dc431fd3dd024b54d75d27887bd63dceb1
Cristianps1988 <cristianps_1@hotmail.com>
2022-12-13 15:33:18 -0500

    imagemin and watch added

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,33 @@
 const {src, dest, watch, series} = require('gulp'); //importar funciones de gulp
+// CSS y SASS
 const sass = require('gulp-sass')(require('sass')); //importar sass y gulp-sass
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+//Imagenes
+const imagemin = require('gulp-imagemin')
 
-function css(done){
+
+function css(){
     //pasos para compilar sass
-    src('src/scss/app.scss') // buscar el archivo a compilar
+    return src('src/scss/app.scss') // buscar el archivo a compilar
         .pipe(sass()) //compilar el archivo
         .pipe(postcss([autoprefixer()]))
         .pipe(dest('build/css')) // guardar el .css
-    done();
+    
 }
 
-function dev() {
-    watch( 'src/css/**/*.scss', css);
+function imagenes(){
+    return src('src/img/**/*')
+    .pipe( imagemin({optimizationLevel: 3}))
+    .pipe(dest('build/img'))
+}
+
+function dev(){
+    watch('src/scss/**/*.scss', css)
+    watch('src/img/**/*', imagenes)
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series( css, dev);
+exports.imagenes = imagenes;
+exports.default = series(imagenes, css, dev);
